86e0d02fa95cb7a5e8498b45a90e61e4dab26bb1
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2021-08-11 22:54:07 -0500

    adds users to courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+user = User.create(email: 'jaime@gmail.com', password: 'testing', password_confirmation: 'testing')
+
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user: user
   }])
 end
