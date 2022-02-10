04863fde8a7128bdf1a394aba0d307f88ba5694f
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-02-10 12:34:34 -0500

    adds index action and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  get '/users', to: 'users#index'
   root 'home#index'
   devise_for :users
   resources :courses
 
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   get 'landing_page', to: 'static_pages#landing_page'
-  
 end
