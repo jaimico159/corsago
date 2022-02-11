e1090ef2fa8dbd7c023490c749034075c47f51bf
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-02-11 18:31:26 -0500

    add users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
 user = User.create(email: 'jaime@gmail.com', password: 'testing', password_confirmation: 'testing')
 
+20.times do
+  User.create(email: Faker::Internet.email, password: 'testing', password_confirmation: 'testing')
+end
+
 30.times do
   Course.create!([{
-    title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote,
-    user_id: User.first.id,
-    short_description: Faker::Quote.famous_last_words,
-    language: Faker::ProgrammingLanguage.name,
-    level: 'Beginner',
-    price: Faker::Number.between(from: 1000, to: 20000)
-  }])
+                   title: Faker::Educator.course_name,
+                   description: Faker::TvShows::GameOfThrones.quote,
+                   user_id: User.first.id,
+                   short_description: Faker::Quote.famous_last_words,
+                   language: Faker::ProgrammingLanguage.name,
+                   level: 'Beginner',
+                   price: Faker::Number.between(from: 1000, to: 20_000)
+                 }])
 end
