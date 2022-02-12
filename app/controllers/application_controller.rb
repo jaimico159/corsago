0ad2be1fca8ab0b3fad770e80758f62437b6f1ee
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-02-12 09:57:14 -0500

    global variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :set_global_variables, if: :user_signed_in?
+  def set_global_variables
+    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) # navbar search
+  end
 end
