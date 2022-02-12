4c118da170337b41a8f72c09d9e29e9b5838d0a5
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-02-11 22:17:53 -0500

    adds constants

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ApplicationRecord
+  LANGUAGES = %i[English Russian Polish Spanish].freeze
+  LEVELS = %i[Beginner Intermediate Advanced].freeze
+
   validates :title, :short_description, :language, :price, :level, presence: true
   validates :description, presence: true, length: { minimum: 5 }
   belongs_to :user
@@ -6,6 +9,14 @@ class Course < ApplicationRecord
 
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  def self.languages
+    LANGUAGES.map { |language| [language, language] }
+  end
+
+  def self.levels
+    LEVELS.map { |level| [level, level] }
+  end
 
   def to_s
     title
